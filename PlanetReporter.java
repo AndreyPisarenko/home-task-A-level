4f5be6da1c642e4cce3bab558e5e9d3d56a8a4b2
pisarenkoAndrey <andreypisarenko98@gmail.com>
2020-08-18 17:22:38 +0300

    lust update

--- PlanetReporter.java
+++ PlanetReporter.java
@@ -2,9 +2,11 @@ package ua.com.solSys;
 
 public class PlanetReporter {
 
-    public static void main(String[] args) {
-        System.out.println();
+    public void USP(Planet Mercury) {
+        System.out.println("USP for" + Mercury.getClass().getSimpleName() + " is " + Mercury.USP());
     }
 
 }
 
+// public void gravitationalAccelerationInfo(Planet planet){
+//        System.out.println("Gravitational Acceleration for " + planet.getClass().getSimpleName() + " is " + planet.getGravitationalAcceleration());
